330b670115db86c265a3ee630609b111af7fb51d
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-01-30 22:23:47 +0200

    corrected link

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,21 @@
 const END_POINT = 'https://restcountries.com/v2/name/';
 
-function fetchCountries(inputData) {
-  return fetch(`${END_POINT}/${inputData}?name,capital,population,flags,languages`).then(res => {
-    // ??? Не знаю як перекинути помилку з потрібним текстом в такому варіанті:
-    // if (!res.ok) {
-    //   console.log(res);
-    //   throw new Error(res.statusText);
-    // }
+// const searchParams = new URLSearchParams({
+//   name,
+// });
 
-    return res.json(); // ТУТ ПРОМІС
-  });
+function fetchCountries(inputData) {
+  return fetch(`${END_POINT}/${inputData}?fields=name,capital,population,flags,languages`).then(
+    res => {
+      // ??? Не знаю як перекинути помилку з потрібним текстом в такому варіанті:
+      // if (!res.ok) {
+      //   console.log(res);
+      //   throw new Error(res.statusText);
+      // }
+
+      return res.json(); // ТУТ ПРОМІС
+    }
+  );
 }
 
 export default { fetchCountries };
